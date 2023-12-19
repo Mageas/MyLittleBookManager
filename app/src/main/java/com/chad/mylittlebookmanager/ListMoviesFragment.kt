54b2b5e23ecffaf9176a7f6a08c543c5c046cc92
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2023-12-19 19:34:04 +0100

    feature: add simple list for static array

--- app/src/main/java/com/chad/mylittlebookmanager/ListMoviesFragment.kt
+++ app/src/main/java/com/chad/mylittlebookmanager/ListMoviesFragment.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.fragment.app.Fragment
 import com.chad.mylittlebookmanager.databinding.FragmentListMoviesBinding
 
 class ListMoviesFragment : Fragment() {
 
-    private lateinit var binding: FragmentListMoviesBinding;
+    private lateinit var binding: FragmentListMoviesBinding
+    private lateinit var listMovies: ListView;
 
     companion object {
         fun newInstance(): ListMoviesFragment {
@@ -23,6 +26,17 @@ class ListMoviesFragment : Fragment() {
     ): View {
         binding = FragmentListMoviesBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        this.listMovies = binding.listMovies
+
+        val simpleArray = arrayOf(1, 2, 3, 4)
+
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, simpleArray)
+        this.listMovies.adapter = adapter
     }
 
 }
