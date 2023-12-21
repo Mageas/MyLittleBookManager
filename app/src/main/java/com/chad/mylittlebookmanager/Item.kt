0c5692d42097006c4ba4654ad3ebba08138d1b90
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2023-12-21 22:30:03 +0100

    feature: rich and morty api

--- app/src/main/java/com/chad/mylittlebookmanager/Item.kt
+++ app/src/main/java/com/chad/mylittlebookmanager/Item.kt
@@ -1,21 +1,33 @@
 package com.chad.mylittlebookmanager
 
-data class Item(
+data class ApiResponse(
+    val info: Info,
+    val results: List<Character>
+)
+
+data class Info(
+    val count: Int,
+    val pages: Int,
+    val next: String?,
+    val prev: String?
+)
+
+data class Character(
     val id: Int,
-    val title: String,
-    val year: Int,
-    val genre: List<String>,
-    val rating: Double,
-    val director: String,
-    val actors: List<String>,
-    val plot: String,
-    val poster: String,
-    val trailer: String,
-    val runtime: Int,
-    val awards: String,
-    val country: String,
-    val language: String,
-    val boxOffice: String,
-    val production: String,
-    val website: String
+    val name: String,
+    val status: String,
+    val species: String,
+    val type: String,
+    val gender: String,
+    val origin: Location,
+    val location: Location,
+    val image: String,
+    val episode: List<String>,
+    val url: String,
+    val created: String
+)
+
+data class Location(
+    val name: String,
+    val url: String
 )
--- app/src/main/java/com/chad/mylittlebookmanager/ListItemsAdapter.kt
+++ app/src/main/java/com/chad/mylittlebookmanager/ListItemsAdapter.kt
@@ -7,19 +7,19 @@ import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 
-class ListItemsAdapter(private val items: List<Item>, private val listener: (Item) -> Unit) : RecyclerView.Adapter<ListItemsAdapter.ItemViewHolder>() {
+class ListItemsAdapter(private val characters: List<Character>, private val listener: (Character) -> Unit) : RecyclerView.Adapter<ListItemsAdapter.ItemViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, position: Int): ItemViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_item, parent, false)
         return ItemViewHolder(view)
     }
 
-    override fun getItemCount(): Int = items.size
+    override fun getItemCount(): Int = characters.size
 
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        val item = items[position]
-        holder.textCustom.text = item.title
+        val item = characters[position]
+        holder.textCustom.text = item.name
         holder.cardItem.setOnClickListener { listener(item) }
     }
 
