490711f8e41d1e5727a0b2291514c6d6ae32d7c2
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2023-12-24 18:11:07 +0100

    feature: add better list items view

--- app/src/main/java/com/chad/mylittlebookmanager/ListItemsAdapter.kt
+++ app/src/main/java/com/chad/mylittlebookmanager/ListItemsAdapter.kt
@@ -1,13 +1,16 @@
 package com.chad.mylittlebookmanager
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
-class ListItemsAdapter(private val characters: List<Character>, private val listener: (Character) -> Unit) : RecyclerView.Adapter<ListItemsAdapter.ItemViewHolder>() {
+class ListItemsAdapter(private val characters: List<Character>, private val favorites: List<Int>, private val listener: (Character) -> Unit) : RecyclerView.Adapter<ListItemsAdapter.ItemViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, position: Int): ItemViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_item, parent, false)
@@ -17,16 +20,31 @@ class ListItemsAdapter(private val characters: List<Character>, private val list
     override fun getItemCount(): Int = characters.size
 
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = characters[position]
-        holder.textCustom.text = item.name
-        holder.cardItem.setOnClickListener { listener(item) }
+        holder.title.text = item.name
+        when (item.status) {
+            "Alive" -> holder.alive.text = "\uD83D\uDFE2"
+            "Dead" -> holder.alive.text = "\uD83D\uDD34"
+            else -> holder.alive.text = "\uD83D\uDFE0"
+        }
+        holder.species.text = item.species
+        holder.favorite.visibility = if (favorites.contains(item.id)) View.VISIBLE else View.INVISIBLE
+        Glide.with(holder.itemView.context)
+            .load(item.image)
+            .into(holder.image)
+        holder.card.setOnClickListener { listener(item) }
     }
 
     class ItemViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView)
     {
-        val textCustom: TextView = itemView.findViewById(R.id.text_custom)
-        val cardItem: CardView = itemView.findViewById(R.id.card_item)
+        val card: CardView = itemView.findViewById(R.id.card_item)
+        val image: ImageView = itemView.findViewById(R.id.list_item_image)
+        val title: TextView = itemView.findViewById(R.id.list_item_title)
+        val favorite: TextView = itemView.findViewById(R.id.list_item_favorite)
+        val alive: TextView = itemView.findViewById(R.id.list_item_alive)
+        val species: TextView = itemView.findViewById(R.id.list_item_species)
     }
 
 }
